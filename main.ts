9efd4c9d18e23abb7c516d4c0befaa176269b99a
pekechamp <86662314+pekechamp@users.noreply.github.com>
2021-06-29 20:33:25 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+	
+})
