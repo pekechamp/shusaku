e7ac4da86922a5eba5784aceebcc6e7dd4218def
pekechamp <86662314+pekechamp@users.noreply.github.com>
2021-07-03 14:55:50 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,59 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        # # # # #
+        `)
+    soundExpression.slide.playUntilDone()
+})
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
     soundExpression.giggle.playUntilDone()
     basic.showString("Hello!")
 })
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        # # # . #
+        . . # # .
+        # # . # #
+        . # # . .
+        # . # # #
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.EigthNote)
+    soundExpression.twinkle.playUntilDone()
+    basic.showString("Anju")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        # . # . #
+        # # # # #
+        `)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
     soundExpression.spring.playUntilDone()
-    basic.showString("  S  A  K  U  M  A")
+    basic.showString("SAKUMA")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     soundExpression.yawn.play()
 })
+basic.showLeds(`
+    . . # # #
+    . . . # #
+    . . . . .
+    # # # . .
+    # # # # #
+    `)
